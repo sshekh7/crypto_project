d80f2586f5e3e28f8aaec50930c285349c1252d6
sshekh7 <49168713+sshekh7@users.noreply.github.com>
2022-12-02 21:16:29 -0500

    init

---
+++ pyteal-course/contracts/escrow/APTTest.py
@@ -0,0 +1,35 @@
+from tinysmpc import VirtualMachine, PrivateScalar, SharedScalar
+from tinysmpc.fixed_point import fixed_point, float_point
+from tinysmpc.finite_ring import MAX_INT64, MIN_INT64
+from tinysmpc.secret_sharing import Share
+from step_01 import create
+
+
+#virtual machines
+alice = VirtualMachine('alice')
+bob = VirtualMachine('bob')
+charlie = VirtualMachine('charlie')
+
+
+#additon and multiplication of SharedScalar
+a = PrivateScalar(19, alice)
+b = PrivateScalar(12, bob)
+c = PrivateScalar(4, charlie)
+
+# Our target is (a*b)+c 
+# Currently uses shared sharing, need to change this to elgamal later
+
+#sharing the generated values
+a_shared = a.share([alice, bob, charlie])
+b_shared = b.share([alice, bob, charlie])
+c_shared = c.share([alice, bob, charlie])
+
+# print(a_shared)
+
+res_mul = a_shared*b_shared
+res_add = res_mul + c_shared
+# print(res_add)
+res = res_add.reconstruct(alice)
+app = create(["A3AGLYMGYMEZ4WMNBPCSMTP2SYDWLMDLNTSJXYU7V5Q77NSCXW5G5EPG6M","SUXUKCFO2V2VFAFU6R3XUT3RVDKLK3BJRBAIAADYHQGZDJFFHMVOBF3CHA","password","2"])
+print(app)
+print(res)
---
+++ pyteal-course/contracts/mpc_smart_contract/smart_contract.py
@@ -0,0 +1,18 @@
+from pyteal import *
+from pyteal.ast.bytes import Bytes
+from pyteal_helpers import program
+
+# pseudo code -- what we need
+# global state:
+# - owner: address
+# - reward
+
+
+
+def approval():
+    return program.event(
+
+    )
+
+def clear():
+    return Approve()
