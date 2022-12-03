72d07762967cb6ed1096a829b19c2619bd9002e8
sshekh7 <49168713+sshekh7@users.noreply.github.com>
2022-12-02 21:17:54 -0500

    Delete elgamal directory

--- elgamal/VirtualMachine.py
+++
@@ -1,9 +0,0 @@
-class VirtualMachine():
-    '''A very simple class that represents a machine's data. 
-       It just has a name and owns objects (PrivateScalars and Shares).'''
-    def __init__(self, name):
-        self.name = name
-        self.objects = []
-    
-    def __repr__(self):
-        return f'VirtualMachine(\'{self.name}\')\n - ' + '\n - '.join(map(str, self.objects))
